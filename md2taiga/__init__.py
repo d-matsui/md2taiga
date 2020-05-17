4d19b531882788496a476808369d0fcb19b8fcec
Daiki Matsui <mti.daiki@gmail.com>
2020-05-17 19:14:24 +0900

    use random SECRET_KEY

--- md2taiga/__init__.py
+++ md2taiga/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+import os
 
 
-def create_app(test_config=None):
+def create_app():
     # create and configure the app
     app = Flask(__name__)
+    app.config.update(
+        SECRET_KEY=os.urandom(24)
+    )
 
     from . import index
     app.register_blueprint(index.bp)
